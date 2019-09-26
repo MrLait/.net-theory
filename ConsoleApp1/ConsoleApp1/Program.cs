c468a9f7befcf46d8b18368eb8641d4c2cbfe540
Lait <mr.lait3@gmail.com>
2019-09-26 14:55:27 +0400

    Приведение типов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,22 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    // Этот тип неявно наследует от типа System.Object
+    internal class Employee : Object
     {
-        static void Main(string[] args)
+    }
+
+    public sealed class Program
+    {
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Приведение типа не требуется, т. к. new возвращает объект Employee,
+            // а Object — это базовый тип для Employee.
+            Object o = new Employee();
+            // Приведение типа обязательно, т. к. Employee — производный от Object
+            // В других языках (таких как Visual Basic) компилятор не потребует
+            // явного приведения
+            Employee e = (Employee)o;
         }
     }
 }
