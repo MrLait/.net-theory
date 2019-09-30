2266687f6704bd66f5371375dfd351569949736c
Lait <mr.lait3@gmail.com>
2019-09-30 11:35:20 +0400

    Пример объявления примитивных типов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft; // Определяем приставку "Microsoft."
-using Wintellect; // Определяем приставку "Wintellect."
-// Имя WintellectWidget определяется как псевдоним для Wintellect.Widget
-using WintellectWidget = Wintellect.Widget;
+﻿using System;
 public sealed class Program
 {
     public static void Main()
     {
-        WintellectWidget w = new WintellectWidget(); // Ошибки нет
+        System.Int32 a = new Int32();
+        int b = 0;
     }
 }
