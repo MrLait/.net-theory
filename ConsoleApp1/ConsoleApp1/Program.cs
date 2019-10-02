c1eddd9125c74c356dea51f50e249665a4cb29fa
Lait <mr.lait3@gmail.com>
2019-10-02 17:47:14 +0400

    Пример использованияполей с разными модификаторами доступа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,17 +2,35 @@
 
 public sealed class Programm
 {
-    public sealed class SomeLibraryType
+    public sealed class SomeType
     {
-        // Модификатор static необходим, чтобы поле
-        // ассоциировалось с типом, а не экземпляром
-        public static readonly Int32 MaxEntriesInList = 50;
+        // Статическое неизменяемое поле. Его значение рассчитывается
+        // и сохраняется в памяти при инициализации класса во время выполнения
+        public static readonly Random s_random = new Random();
+        // Статическое изменяемое поле
+        private static Int32 s_numberOfWrites = 0;
+        //Неизменяемое экземплярное поле
+        public readonly String Pathname = "Untitled";
+        // Изменяемое экземплярное поле
+        private System.IO.FileStream m_fs;
+
+        public SomeType(String patname)
+        {
+            // Эта строка изменяет значение неизменяемого поля
+            // В данном случае это возможно, так как показанный далее код
+            // расположен в конструкторе
+            Pathname = patname;
+        }
+        public String DoSomething()
+        {
+            // Эта строка читает и записывает значение статического изменяемого поля
+            s_numberOfWrites = s_numberOfWrites + 1;
+            // Эта строка читает значение неизменяемого экземплярного поля
+            return Pathname;
+        }
     }
     public static void Main()
     {
-        Console.WriteLine("Max entries supported in list: "
-        + SomeLibraryType.MaxEntriesInList);
-
         Console.ReadKey();
     }
 }
