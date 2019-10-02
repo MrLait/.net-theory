47640afd2144b78cb4d984fae54a5bd6e0d931f8
Lait <mr.lait3@gmail.com>
2019-10-02 16:39:38 +0400

    Объявленеи константы Empty с типом SomeType и значением null

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,49 +1,15 @@
-﻿using CompanyA;
-using CompanyB;
-using System;
-
-namespace CompanyA
-{
-    public class Phone
-    {
-        public void Dial()
-        {
-            Console.WriteLine("Phone.Dial");
-            EstablishConnection();
-            //Выполнить действия по набору номера
-        }
-        protected virtual void EstablishConnection()
-        {
-            Console.WriteLine("Phone.EstablishConnection");
-            // Выполнить действия по установлению соединения
-        }
-    }
-}
-//А теперь представьте, что в компании CompanyB спроектировали другой тип,
-//BetterPhone, использующий тип Phone в качестве базового:
-namespace CompanyB
-{
-    public class BetterPhone : Phone
-    {
-        // Метод Dial удален (так как он наследуется от базового типа)
-
-        // Здесь ключевое слово new удалено, а модификатор virtual заменен
-        // на override, чтобы указать, что этот метод связан с методом
-        // EstablishConnection из базового типа
-        protected override void EstablishConnection()
-        {
-            Console.WriteLine("BetterPhone.EstablishConnection");
-            // Выполнить действия по набору телефонного номера
-        }
-    }
-}
+﻿using System;
 
 public sealed class Programm
 {
-    public static void Main()
+    public sealed class SomeType
     {
-        BetterPhone p2 = new BetterPhone();
-        p2.Dial();
-        Console.ReadKey();
+        // Некоторые типы не являются элементарными, но С# допускает существование
+        // константных переменных этих типов после присваивания значения null
+        public const SomeType Empty = null;
     }
-}
+        public static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
