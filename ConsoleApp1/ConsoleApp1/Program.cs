7d31fa70e0e14a88159aadd6fbbf91d313135ebd
Lait <mr.lait3@gmail.com>
2019-10-02 14:49:02 +0400

    Вызов виртуального метода как невиртуального ключевое слово sealed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,64 +1,26 @@
 ﻿using System;
-internal class Employee
-{
-    //Невиртуальный экземплярный метод
-    public Int32 GetYearsEmployee(){ return 0; }
-    //Виртуальный метод (виртуальный -значит, экземплярный)
-    public virtual String GetProgressReport() { return null; }
-    //Статический метод
-    public static Employee Lookup(string name) { return null; }
-}
-
 public sealed class Program
 {
+    public sealed class Point
+    {
+        private Int32 m_x, m_y;
+        public Point(Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0},{1}", m_x, m_y);
+        }
+    }
     public static void Main()
     {
-        Console.WriteLine(); //вызов сатического метода
-        Object o = new Object();
-        o.GetHashCode(); //Вызов виртуального экземплярного метода
-        o.GetType(); // Вызов невиртуального экземплярного метода
-
-        Program p = null;
-        p.GetFive(); // В C# выдается NullReferenceException
-    }
-    //Невиртуального экземплярный метод
-    public Int32 GetFive() { return 5; }
-}
-
-internal class SomeClass
-{
-    //ToString виртуальный метод базового класса Object
-    public override String ToString()
-    {
-        // Компилятор использует команду call для невиртуального вызова
-        // метода ToString класса Object
-
-        // Если бы компилятор вместо call использовал callvirt, этот
-        // метод продолжал бы рекурсивно вызывать сам себя до переполнения стека
-        return base.ToString();
+        Point p = new Point(3, 4);
+        // Компилятор C# вставит здесь инструкцию callvirt,
+        // но JIT-компилятор оптимизирует этот вызов и сгенерирует код
+        // для невиртуального вызова ToString,
+        // поскольку p имеет тип Point, являющийся запечатанным
+        Console.WriteLine(p);
     }
 }
-
-public class Set
-{
-    private Int32 m_length = 0;
-    // Этот перегруженный метод — невиртуальный
-    public Int32 Find(Object value)
-    {
-        return Find(value, 0, m_length);
-    }
-    // Этот перегруженный метод — невиртуальный
-    public Int32 Find(Object value, Int32 startIndex)
-    {
-        return Find(value, startIndex, m_length);
-    }
-    // Наиболее функциональный метод сделан виртуальным
-    // и может быть переопределен
-    public virtual Int32 Find(Object value, Int32 startIndex, Int32 endIndex)
-    {
-        // Здесь находится настоящая реализация, которую можно переопределить...
-        return 0;
-    }
-    // Другие методы
-}
-
