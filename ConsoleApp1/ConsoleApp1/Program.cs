d610e52f5fb59ae6f9c00d340442d3f156a7a381
Lait <mr.lait3@gmail.com>
2019-10-02 17:34:49 +0400

    Решение проблемы с управлениями версий, по сравнение с константами

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,16 +4,15 @@ public sealed class Programm
 {
     public sealed class SomeLibraryType
     {
-        // ПРИМЕЧАНИЕ: C# не позволяет использовать для констант модификатор
-        // static, поскольку всегда подразумевается, что константы являются
-        // статическими
-        public const Int32 MaxEntriesInList = 50;
+        // Модификатор static необходим, чтобы поле
+        // ассоциировалось с типом, а не экземпляром
+        public static readonly Int32 MaxEntriesInList = 50;
     }
     public static void Main()
     {
         Console.WriteLine("Max entries supported in list: "
         + SomeLibraryType.MaxEntriesInList);
-        
+
         Console.ReadKey();
     }
 }
