ce81c4f005e92814c1b090fd7db4896586546bf7
Lait <mr.lait3@gmail.com>
2019-10-03 21:39:57 +0400

    тот же код но не partial v3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,32 +1,16 @@
 ﻿using System;
-// Сгенерированный код в некотором файле с исходным кодом: 
-internal sealed partial class Base
+// Логический эквивалент сгенерированного инструментом кода в случае,
+// когда нет объявления выполняемого частичного метода
+internal sealed class Base
 {
     private String m_name;
-    // Это объявление с определением частичного метода вызывается
-    // перед изменением поля m_name
-    partial void OnNameChanging(String value);
-
     public String Name
     {
         get { return m_name; }
         set
         {
-            //Информирование класса о потенциальном изменении
-            OnNameChanging(value.ToUpper());
             m_name = value; // Изменение поля
         }
-    }
-}
-// Написанный программистом код из другого файла 
-internal sealed partial class Base
-{
-    // Это объявление с реализацией частичного метода вызывается перед тем,
-    // как будет изменено поле m_name
-    partial void OnNameChanging(String value)
-    {
-        if (String.IsNullOrEmpty(value))
-        throw new ArgumentNullException("value");
     }
 }
 
