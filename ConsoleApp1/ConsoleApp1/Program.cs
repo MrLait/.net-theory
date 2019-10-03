1a221704d55cea7b2bdc7bb045c30ae8e0c0dac3
Lait <mr.lait3@gmail.com>
2019-10-03 17:31:37 +0400

    Методы операторов преобразования

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 
-public sealed class Complex
+public sealed class Rational
 {
-    public static Complex operator+(Complex c1,Complex c2)
-    {
-        return (c1 + c2);
-    }
-    //или так
-    public static Complex Add(Complex c1, Complex c2)
-    {
-        return (c1 + c2);
-    }
+    // Создает Rational из Int32
+    public Rational(Int32 num) { }
+    // Создает Rational из Single
+    public Rational(Single num) { }
+    // Преобразует Rational в Int32
+    public Int32 ToInt32() { }
+    // Преобразует Rational в Single
+    public Single ToSingle(){}
 }
 
 public sealed class Programm
 {
     public static void Main()
     {
-        Complex c = new Complex();
         //SomeType p = new SomeType();
         //Console.WriteLine(p);
         Console.ReadKey();
