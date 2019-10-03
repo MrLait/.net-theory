97dd97cde2a235c87f5f1d5b034f25b6931c5866
Lait <mr.lait3@gmail.com>
2019-10-03 14:44:34 +0400

    Явынй конструктор struct

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,67 +1,20 @@
 ﻿using System;
 
-public class SomeType { }
-
-//Это определение идентично определению:
-
-public class SomeTyp2
+internal struct Point
 {
-    public SomeTyp2() : base() { }
-}
-
-internal sealed class SomeType3
-{
-    private Int32 m_x = 5;
-}
-
-internal sealed class SomeType4
-{
-    private Int32 m_x = 5;
-    private String m_s = "Hi";
-    private Double m_d = 3.14;
-    private Byte m_b;
-    //Это конструкторы
-    public SomeType4() { }
-    public SomeType4(Int32 x) { }
-    public SomeType4(String s)
-    {
-        m_d = 10;
-    }
-}
-
-internal sealed class SomeType5
-{
-    // Здесь нет кода, явно инициализирующего поля
-    private Int32 m_x;
-    private String m_s;
-    private Double m_d;
-    private Byte m_b;
-    // Код этого конструктора инициализирует поля значениями по умолчанию
-    // Этот конструктор должен вызываться всеми остальными конструкторами
-    public SomeType5()
-    {
-        m_x = 5;
-        m_s ="Hi";
-        m_d = 3.14;
-        m_b = 0xff;
-    }
-    // Этот конструктор инициализирует поля значениями по умолчанию,
-    // а затем изменяет значение m_x
-    public SomeType5(Int32 x) : this()
+    public Int32 m_x, m_y;
+    public Point(Int32 x, Int32 y)
     {
         m_x = x;
+        m_y = y;
     }
-    // Этот конструктор инициализирует поля значениями по умолчанию,
-    // а затем изменяет значение m_s
-    public SomeType5(String s) : this()
+}
+internal sealed class Rectangle
+{
+    public Point m_topLeft, m_bottonRight;
+    public Rectangle(Point topLeft, Point buttonRight)
     {
-        m_s = s;
-    }
-    // Этот конструктор инициализирует поля значениями по умолчанию,
-    // а затем изменяет значения m_x и m_s
-    public SomeType5(Int32 x, String s) : this()
-    {
-        m_x = x;
-        m_s = s;
+        m_topLeft = topLeft;
+        m_bottonRight = buttonRight;
     }
 }
