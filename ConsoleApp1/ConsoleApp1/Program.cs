d96df20841e5eafc00f42676e0b827eb95b5a1f7
Lait <mr.lait3@gmail.com>
2019-10-03 14:54:29 +0400

    в структуре не могут содержаться инициализаторы свойств или полей экземпляров.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,6 @@
 ﻿using System;
 
-internal struct Point
+internal struct SomeValType
 {
-    public Int32 m_x, m_y;
-    public Point()
-    {
-        m_x = m_y = 5;
-    }
-}
-internal sealed class Rectangle
-{
-    public Point m_topLeft, m_bottonRight;
-    public Rectangle()
-    {
-    }
+    public Int32 m_x, m_y = 5;
 }
