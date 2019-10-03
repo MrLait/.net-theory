6e5e29a931d0d94060d12cac571be029f1b48873
Lait <mr.lait3@gmail.com>
2019-10-03 18:24:08 +0400

    Неявное и явное приведение Int32 к Rational и наоборот. полностью рабочая программа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,14 +2,19 @@
 
 public sealed class Rational
 {
+    private Int32 num1;
+    private Single num2;
     // Создает Rational из Int32
-    public Rational(Int32 num) { }
+    public Rational(Int32 num) 
+    { 
+        num1 = num; 
+    }
     // Создает Rational из Single
-    public Rational(Single num) { }
+    public Rational(Single num) { num2 = num; }
     // Преобразует Rational в Int32
-    public Int32 ToInt32() { }
+    public Int32 ToInt32(){ return num1; }
     // Преобразует Rational в Single
-    public Single ToSingle(){}
+    public Single ToSingle(){ return num2; }
     // Неявно создает Rational из Int32 и возвращает полученный объект
     public static implicit operator Rational(Int32 num)
     {
@@ -31,13 +36,13 @@ public sealed class Rational
         return rational.ToSingle();
     }
 }
-
 public sealed class Programm
 {
     public static void Main()
     {
-        //SomeType p = new SomeType();
-        //Console.WriteLine(p);
-        Console.ReadKey();
+        Rational r1 = 5; // Неявное приведение Int32 к Rational
+        Rational r2 = 2.5F; // Неявное приведение Single к Rational
+        Int32 x = (Int32)r1; // Явное приведение Rational к Int32
+        Single y = (Single)r2; // Явное приведение Rational к Single
     }
 }
