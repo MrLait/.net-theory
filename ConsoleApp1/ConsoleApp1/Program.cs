8714f204a1de7378990d623eda398a0b2ad2f86b
Lait <mr.lait3@gmail.com>
2019-10-03 15:36:33 +0400

    инициализация экземплярных поля

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,29 +1,15 @@
 ﻿using System;
 
-internal sealed class SomeRefType
+internal sealed class SomeType
 {
-    static SomeRefType()
-    {
-        // Исполняется при первом обращении к ссылочному типу SomeRefType}
-    }
-}
-internal struct SomeValType
-{
-    // C# на самом деле допускает определять для значимых типов
-    // конструкторы без параметров
-    static SomeValType()
-    {
-        // Исполняется при первом обращении к значимому типу SomeValType}
-        Console.WriteLine("This never gets displayed");
-    }
-    public Int32 m_x;
+    public Int32 m_x = 5;
 }
 
 public sealed class Programm
 {
     public static void Main()
     {
-        SomeValType p = new SomeValType();
+        SomeType p = new SomeType();
         Console.WriteLine(p.m_x);
         p.m_x = 100;
         Console.WriteLine(p.m_x);
