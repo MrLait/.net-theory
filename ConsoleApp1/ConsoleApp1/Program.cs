9f3328b696f92c10e66a820726e164445183db18
Lait <mr.lait3@gmail.com>
2019-10-03 17:41:26 +0400

    продолжение к прошлому. использование ключевых слов implicit и explicit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,26 @@ public sealed class Rational
     public Int32 ToInt32() { }
     // Преобразует Rational в Single
     public Single ToSingle(){}
+    // Неявно создает Rational из Int32 и возвращает полученный объект
+    public static implicit operator Rational(Int32 num)
+    {
+        return new Rational(num);
+    }
+    // Неявно создает Rational из Single и возвращает полученный объект
+    public static implicit operator Rational(Single num)
+    {
+        return new Rational(num);
+    }
+    // Явно возвращает объект типа Int32, полученный из Rational
+    public static explicit operator Int32(Rational rational)
+    {
+        return rational.ToInt32();
+    }
+    // Явно возвращает объект типа Single, полученный из Rational
+    public static explicit operator Single(Rational rational)
+    {
+        return rational.ToSingle();
+    }
 }
 
 public sealed class Programm
