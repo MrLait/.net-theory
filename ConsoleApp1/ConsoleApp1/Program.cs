f21163ee4d6305e11f25fec7eeb71b14f06ad82a
Lait <mr.lait3@gmail.com>
2019-10-03 14:57:45 +0400

    И даже так будет ошибка

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,5 +2,10 @@
 
 internal struct SomeValType
 {
-    public Int32 m_x, m_y = 5;
+    public Int32 m_x, m_y;
+    // C# допускает наличие у значимых типов конструкторов с параметрами
+    public SomeValType(Int32 x)
+    {
+        m_x = x;
+    }
 }
