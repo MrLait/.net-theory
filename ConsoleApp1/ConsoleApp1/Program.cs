d87433e452f3e94979d380455c70afa0b29b9f1e
Lait <mr.lait3@gmail.com>
2019-10-03 21:32:55 +0400

    v2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,29 +1,40 @@
 ﻿using System;
 // Сгенерированный код в некотором файле с исходным кодом: 
-internal class Base
+internal sealed partial class Base
 {
     private String m_name;
-    // Вызывается перед изменением поля m_name 
-    protected virtual void OnNameChanging(String value) 
-    { }
+    // Это объявление с определением частичного метода вызывается
+    // перед изменением поля m_name
+    partial void OnNameChanging(String value);
+
     public String Name
     {
         get { return m_name; }
         set
         {
-            // Информирует класс о возможных изменениях 
+            //Информирование класса о потенциальном изменении
             OnNameChanging(value.ToUpper());
-            m_name = value; // Изменение поля 
+            m_name = value; // Изменение поля
         }
     }
 }
 // Написанный программистом код из другого файла 
-internal class Derived : Base
+internal sealed partial class Base
 {
-    protected override void OnNameChanging(string value)
+    // Это объявление с реализацией частичного метода вызывается перед тем,
+    // как будет изменено поле m_name
+    partial void OnNameChanging(String value)
     {
         if (String.IsNullOrEmpty(value))
-            throw new ArgumentNullException("value");
+        throw new ArgumentNullException("value");
     }
 }
 
+public sealed class Programm
+{
+    public static void Main()
+    {
+        Base d = new Base { Name = "s" };
+        d.Name = "ss";
+    }
+}
