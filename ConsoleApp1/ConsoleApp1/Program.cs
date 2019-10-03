970cff96d7e45172f0e98102572b74739f9308fa
Lait <mr.lait3@gmail.com>
2019-10-03 15:21:46 +0400

    CLR не вызывает статический конструктор значимого типа.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 
+internal sealed class SomeRefType
+{
+    static SomeRefType()
+    {
+        // Исполняется при первом обращении к ссылочному типу SomeRefType}
+    }
+}
 internal struct SomeValType
 {
-    public Int32 m_x, m_y;
-    // C# позволяет значимым типам иметь конструкторы с параметрами
-    public SomeValType(Int32 x)
+    // C# на самом деле допускает определять для значимых типов
+    // конструкторы без параметров
+    static SomeValType()
     {
-        // Выглядит необычно, но компилируется прекрасно,
-        // и все поля инициализируются значениями 0 или null
-        this = new SomeValType();
-        m_x = x; // Присваивает m_x значение x
-    // Обратите внимание, что поле m_y было инициализировано нулем
+        // Исполняется при первом обращении к значимому типу SomeValType}
+        Console.WriteLine("This never gets displayed");
+    }
+    public Int32 m_x;
+}
+
+public sealed class Programm
+{
+    public static void Main()
+    {
+        SomeValType p = new SomeValType();
+        Console.WriteLine(p.m_x);
+        p.m_x = 100;
+        Console.WriteLine(p.m_x);
+
+        Console.ReadKey();
     }
 }
