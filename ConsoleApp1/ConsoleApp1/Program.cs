9d631dc9a2ef27c38d7f9ec849841ccc487b80cb
Lait <mr.lait3@gmail.com>
2019-10-03 16:05:25 +0400

    Инициализация статического поля и одновременно инициализация поля в конструкторе типа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,19 +2,18 @@
 
 internal sealed class SomeType
 {
-    private static Int32 m_x;
+    private static Int32 m_x = 5;
     static SomeType()
     {
-        m_x = 5;
+        m_x = 10;
     }
 }
-
 public sealed class Programm
 {
     public static void Main()
     {
         SomeType p = new SomeType();
-
+        Console.WriteLine(p);
         Console.ReadKey();
     }
 }
