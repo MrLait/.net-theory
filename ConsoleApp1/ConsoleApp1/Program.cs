fa8d1e6ef918c83e5b959564c57baa9da3fc7a21
Lait <mr.lait3@gmail.com>
2019-10-04 15:06:51 +0400

    Пример использования параметров по умолчанию и именованных параметров, в которые явно передаются аргументы

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
-// Логический эквивалент сгенерированного инструментом кода в случае,
-// когда нет объявления выполняемого частичного метода
-internal sealed class Base
+
+public static class Programm
 {
-    private String m_name;
-    public String Name
+    private static Int32 s_n = 0;
+    private static void M(Int32 x = 9, String s = "A", 
+        DateTime dt = default(DateTime), Guid guid = new Guid())
+    //или так Guid guid = default(Guid)
+    //или даже так Guid guid = default
     {
-        get { return m_name; }
-        set
-        {
-            m_name = value; // Изменение поля
-        }
+        Console.WriteLine("x={0}, s={1}, dt={2}, guid={3}", x, s, dt, guid);
+    }
+    public static void Main()
+    {
+        // 1. Аналогично: M(9, "A", default(DateTime), new Guid());
+        M();
+        // 2. Аналогично: M(8, "X", default(DateTime), new Guid());
+        M(8, "X");
+        // 3. Аналогично: M(5, "A", DateTime.Now, Guid.NewGuid());
+        M(5, dt: DateTime.Now, guid: Guid.NewGuid());
+        // 4. Аналогично: M(0, "1", default(DateTime), new Guid());
+        M(s_n++, s_n++.ToString());
+        // 5. Аналогично: String t1 = "2"; Int32 t2 = 3;
+        M(s: s_n++.ToString(), x: s_n++);
     }
 }
 
-public sealed class Programm
-{
-    public static void Main()
-    {
-        Base d = new Base { Name = "s" };
-        d.Name = "ss";
-    }
-}
