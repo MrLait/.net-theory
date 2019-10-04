a8edc47ad77ec2816520b9e0cb26c787a6949d0b
Lait <mr.lait3@gmail.com>
2019-10-04 15:31:19 +0400

    Еще фишки связанные  необяхательными и именованными аргументами

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,13 +3,7 @@
 public static class Programm
 {
     private static Int32 s_n = 0;
-    private static void M(Int32 x = 9, String s = "A", 
-        DateTime dt = default(DateTime), Guid guid = new Guid())
-    //или так Guid guid = default(Guid)
-    //или даже так Guid guid = default
-    {
-        Console.WriteLine("x={0}, s={1}, dt={2}, guid={3}", x, s, dt, guid);
-    }
+    
     public static void Main()
     {
         // 1. Аналогично: M(9, "A", default(DateTime), new Guid());
@@ -22,6 +16,37 @@ public static class Programm
         M(s_n++, s_n++.ToString());
         // 5. Аналогично: String t1 = "2"; Int32 t2 = 3;
         M(s: s_n++.ToString(), x: s_n++);
+        Console.WriteLine(MakePath());
+        Console.WriteLine(MakePath2());
+        
+        // Вызов метода:
+        Int32 a = 5;
+        M2(x: ref a);
+    }
+    // Объявление метода:
+    private static void M2(ref Int32 x)
+    {
+        Console.WriteLine("x = {0}", x);
+    }
+
+    // Не делайте так:
+    private static String MakePath(String filename = "Untitled")
+    {
+        return String.Format(@"C:\{0}.txt", filename);
+    }
+    // Используйте следующее решение:
+    private static String MakePath2(String fileName = null)
+    {
+        // Здесь применяется оператор, поддерживающий
+        // значение null (??); см. главу 19
+        return String.Format(@"C:\{0}.txt", fileName ?? "Untitled");
+    }
+    private static void M(Int32 x = 9, String s = "A",
+        DateTime dt = default(DateTime), Guid guid = new Guid())
+    //или так Guid guid = default(Guid)
+    //или даже так Guid guid = default
+    {
+        Console.WriteLine("x={0}, s={1}, dt={2}, guid={3}", x, s, dt, guid);
     }
 }
 
