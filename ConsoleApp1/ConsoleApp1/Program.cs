eb978dd0bc69ce259217e1d3185e6aa321ffc39e
Lait <mr.lait3@gmail.com>
2019-10-04 15:45:20 +0400

    Неявно типизированные локальные переменные

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,52 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Programm
 {
-    private static Int32 s_n = 0;
-    
     public static void Main()
     {
-        // 1. Аналогично: M(9, "A", default(DateTime), new Guid());
-        M();
-        // 2. Аналогично: M(8, "X", default(DateTime), new Guid());
-        M(8, "X");
-        // 3. Аналогично: M(5, "A", DateTime.Now, Guid.NewGuid());
-        M(5, dt: DateTime.Now, guid: Guid.NewGuid());
-        // 4. Аналогично: M(0, "1", default(DateTime), new Guid());
-        M(s_n++, s_n++.ToString());
-        // 5. Аналогично: String t1 = "2"; Int32 t2 = 3;
-        M(s: s_n++.ToString(), x: s_n++);
-        Console.WriteLine(MakePath());
-        Console.WriteLine(MakePath2());
-        
-        // Вызов метода:
-        Int32 a = 5;
-        M2(x: ref a);
+        ImplicitlyTypedLocalVariables();
     }
-    // Объявление метода:
-    private static void M2(ref Int32 x)
+    private static void ImplicitlyTypedLocalVariables()
     {
-        Console.WriteLine("x = {0}", x);
+        var name = "Jeff";
+        ShowVariableType(name); // Вывод: System.String
+        //Не удается присвоить<NULL> неявно типизированной переменной.
+        //var test = null; 
+        var x = (String)null; // Допустимо, хотя и бесполезно
+        ShowVariableType(x); // Вывод: System.String
+        var number = new Int32[] { 1, 2, 3 };
+        ShowVariableType(number); // Вывод: System.Int32[]
+        // Меньше символов при вводе сложных типов
+        var collection = new Dictionary<String, Single>() { { "Grand", 4.0F } };
+        // Вывод: System.Collections.Generic.Dictionary`2[System.String,System.Single]
+        ShowVariableType(collection);
+        foreach (var item in collection)
+        {
+            // Вывод: System.Collections.Generic.KeyValuePair`2
+            //[System.String, System.Single]
+            ShowVariableType(item);
+        }
     }
-
-    // Не делайте так:
-    private static String MakePath(String filename = "Untitled")
+    private static void ShowVariableType<T>(T t)
     {
-        return String.Format(@"C:\{0}.txt", filename);
-    }
-    // Используйте следующее решение:
-    private static String MakePath2(String fileName = null)
-    {
-        // Здесь применяется оператор, поддерживающий
-        // значение null (??); см. главу 19
-        return String.Format(@"C:\{0}.txt", fileName ?? "Untitled");
-    }
-    private static void M(Int32 x = 9, String s = "A",
-        DateTime dt = default(DateTime), Guid guid = new Guid())
-    //или так Guid guid = default(Guid)
-    //или даже так Guid guid = default
-    {
-        Console.WriteLine("x={0}, s={1}, dt={2}, guid={3}", x, s, dt, guid);
+        Console.WriteLine(typeof(T));
     }
 }
 
