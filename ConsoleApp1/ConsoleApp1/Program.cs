2298a57f8d33b94072f1d786b9f618ed5f161ae0
Lait <mr.lait3@gmail.com>
2019-10-07 17:50:12 +0400

    Решение снижения производительности из-за переменного числа аргументов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,63 @@
 ﻿using System;
 public sealed class Programm
 {
+    public sealed class DisplayTypes : Object
+    {
+
+        public static void Display(object? arg0, object? arg1, object? arg2, object? arg3)
+        {
+            Object[] args = { arg0, arg1, arg2, arg3 };
+
+            if (args != null)
+            {
+                foreach (var o in args)
+                {
+                    Console.WriteLine(o.GetType());
+                }
+            }
+        }
+        public static void Display(object? arg0, object? arg1, object? arg2)
+        {
+            Object[] args = { arg0, arg1, arg2 };
+            if (args != null)
+            {
+                foreach (var o in args)
+                {
+                    Console.WriteLine(o.GetType());
+                }
+            }
+        }
+        public static void Display(object? arg0, object? arg1)
+        {
+            Object[] args = { arg0, arg1};
+            if (args != null)
+            {
+                foreach (var o in args)
+                {
+                    Console.WriteLine(o.GetType());
+                }
+            }
+        }
+        public static void Display(object? arg0)
+        {
+            Console.WriteLine(arg0.GetType());
+        }
+        public static void Display(params object?[] args)
+        {
+            if (args != null)
+            {
+                foreach (var o in args)
+                {
+                    Console.WriteLine(o.GetType());
+                }
+            }
+        }
+    }
     public static void Main()
     {
         Console.WriteLine(Add(new Int32[] { 1, 2, 3, 4, 5 }));
         //Благодаря ключевому слову params такой вызов доступен
-        Console.WriteLine(Add(1,2,3,4,5));
+        Console.WriteLine(Add(1, 2, 3, 4, 5));
         // Обе строчки выводят "0" 
         // передает новый элемент Int32[0] методу Add 
         Console.WriteLine(Add());
@@ -13,19 +65,14 @@ public sealed class Programm
         // что более эффективно (не выделяется } 
         // память под массив)
         Console.WriteLine(Add(null));
-        DisplayTypes(new Object(), new Random(), "Jeff", 5);
-        }
-
-    private static void DisplayTypes(params Object[] objects)
-    {
-        if (objects != null)
-        {
-            foreach (var o in objects)
-            {
-                Console.WriteLine(o.GetType());
-            }
-        }
+        DisplayTypes.Display(new Object(), new Random(), "Jeff", 5);
     }
+
+    //private static void DisplayTypes(object v1, Random random, string v2, int v3)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
     static Int32 Add(params Int32[] values)
     {
         // ПРИМЕЧАНИЕ: при необходимости этот массив
@@ -40,5 +87,6 @@ public sealed class Programm
         }
         return sum;
     }
+
 }
 
