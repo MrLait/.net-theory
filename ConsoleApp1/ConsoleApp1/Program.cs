35e9d76c5a78c19206e3de35b5d963849368500f
Lait <mr.lait3@gmail.com>
2019-10-07 16:04:57 +0400

    Пример ипользования методов с параметрами ref out

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-
+using System.IO;
 public sealed class Programm
 {
     public static void Main()
     {
-        Int32 x = 5; // Инициализация х
-        AddVal(ref x); // x требуется инициализировать
-        Console.WriteLine(x); 
+        FileStream fs; // Объект fs не инициализирован
+        // Первый файл открывается для обработки
+        StartProcessingFiles(out fs);
+        // Продолжаем, пока остаются файлы для обработки
+        for (;  fs != null; ContinueProcessingFiles(ref fs))
+        {
+            Byte[] bytes = new Byte[fs.Length];
+            //Обработка файла
+            fs.Read(bytes);
+        }
     }
-    private static void AddVal(ref Int32 x)
+    private static void StartProcessingFiles(out FileStream fs)
     {
-        x += 10;
+        fs = new FileStream("Path", FileMode.Open); // в этом методе объект fs
+                                                    // должен инициализироваться
+    }
+    private static void ContinueProcessingFiles(ref FileStream fs)
+    {
+        fs.Close(); // Закрытие последнего обрабатываемого файла
+        // Открыть следующий файл или вернуть null, если файлов больше нет
+        if (noMoreFilesToProcess) fs = null;
+        else fs = new FileStream("newPath", FileMode.Open);
     }
 }
 
