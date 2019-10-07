e77e837d1b25494e89fa2cf9c023560245804100
Lait <mr.lait3@gmail.com>
2019-10-07 17:14:36 +0400

    Добавлен метод, принимающий произвольное количество параметров любого типа

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,27 @@ public sealed class Programm
     public static void Main()
     {
         Console.WriteLine(Add(new Int32[] { 1, 2, 3, 4, 5 }));
+        //Благодаря ключевому слову params такой вызов доступен
         Console.WriteLine(Add(1,2,3,4,5));
+        // Обе строчки выводят "0" 
+        // передает новый элемент Int32[0] методу Add 
+        Console.WriteLine(Add());
+        // передает методу Add значение null, 
+        // что более эффективно (не выделяется } 
+        // память под массив)
+        Console.WriteLine(Add(null));
+        DisplayTypes(new Object(), new Random(), "Jeff", 5);
+        }
+
+    private static void DisplayTypes(params Object[] objects)
+    {
+        if (objects != null)
+        {
+            foreach (var o in objects)
+            {
+                Console.WriteLine(o.GetType());
+            }
+        }
     }
     static Int32 Add(params Int32[] values)
     {
