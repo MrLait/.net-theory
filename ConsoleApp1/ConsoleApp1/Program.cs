9fbeaba3fba087169bc10aa11ea19ad68dd271bc
Lait <mr.lait3@gmail.com>
2019-10-07 16:58:54 +0400

    Использование ключевого слова params Вызов прошлого написанного метода

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ public sealed class Programm
 {
     public static void Main()
     {
+        Console.WriteLine(Add(new Int32[] { 1, 2, 3, 4, 5 }));
+        Console.WriteLine(Add(1,2,3,4,5));
     }
     static Int32 Add(params Int32[] values)
     {
