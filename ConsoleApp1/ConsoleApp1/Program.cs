8a37f08fc917e5de52eb94fcc4b4443c40a673b7
Lait <mr.lait3@gmail.com>
2019-10-07 15:34:58 +0400

    out

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,36 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 
-public static class Programm
+public sealed class Programm
 {
     public static void Main()
     {
-        ImplicitlyTypedLocalVariables();
+        Int32 x; // Инициализация х
+        GetVal(out x); // Инициализация х не обязательна
+        Console.WriteLine(x); 
     }
-    private static void ImplicitlyTypedLocalVariables()
+    private static void GetVal(out Int32 x)
     {
-        var name = "Jeff";
-        ShowVariableType(name); // Вывод: System.String
-        //Не удается присвоить<NULL> неявно типизированной переменной.
-        //var test = null; 
-        var x = (String)null; // Допустимо, хотя и бесполезно
-        ShowVariableType(x); // Вывод: System.String
-        var number = new Int32[] { 1, 2, 3 };
-        ShowVariableType(number); // Вывод: System.Int32[]
-        // Меньше символов при вводе сложных типов
-        var collection = new Dictionary<String, Single>() { { "Grand", 4.0F } };
-        // Вывод: System.Collections.Generic.Dictionary`2[System.String,System.Single]
-        ShowVariableType(collection);
-        foreach (var item in collection)
-        {
-            // Вывод: System.Collections.Generic.KeyValuePair`2
-            //[System.String, System.Single]
-            ShowVariableType(item);
-        }
-    }
-    private static void ShowVariableType<T>(T t)
-    {
-        Console.WriteLine(typeof(T));
+        x = 10;
     }
 }
 
