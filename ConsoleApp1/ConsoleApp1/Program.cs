d357153d85a7932512b3b936dfaf35bd3fb3e310
Lait <mr.lait3@gmail.com>
2019-10-08 17:54:53 +0400

    Пример использования Tuple НЕ лучшая практика

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,29 +4,19 @@ using System.IO;
 
 public sealed class Programm
 {
+
+
     public static void Main()
     {
+        var minmax = MinMax(6, 2);
+        Console.WriteLine("Min={0}, Max={1}", 
+            minmax.Item1, minmax.Item2); // Min=2, Max=6
     }
-    // Рекомендуется в этом методе использовать параметр слабого типа
-    public void ManipulateItems<T>(IEnumerable<T> collection) { }
-    // Не рекомендуется в этом методе использовать параметр сильного типа
-    public void ManipulateItems<T>(List<T> collection) { }
-    // Рекомендуется в этом методе использовать параметр мягкого типа
-    public void ProcessBytes(Stream someStream){}
-    // Не рекомендуется в этом методе использовать параметр сильного типа
-    public void ProcessBytes(FileStream someStream) {}
 
-    // Рекомендуется в этом методе использовать
-    // сильный тип возвращаемого объекта
-    public FileStream openFile() { return new FileStream("path", FileMode.Open); }
-    // Не рекомендуется в этом методе использовать
-    // слабый тип возвращаемого объекта
-    public Stream OpenFile() { return Stream.Null; }
-    // Гибкий вариант: в этом методе используется
-    // мягкий тип возвращаемого объекта
-    public IList<Stream> GetStringCollection() { return null; }
-    // Негибкий вариант: в этом методе используется
-    // сильный тип возвращаемого объекта
-    public List<Stream> GetStringCollection() { return null; }
+    // Возвращает минимум в Item1 и максимум в Item2
+    private static Tuple<Int32, Int32> MinMax(Int32 a, Int32 b)
+    {
+        return new Tuple<Int32, Int32>(Math.Min(a, b), Math.Max(a, b));
+    }
 }
 
