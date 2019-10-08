e32b7ff3996eafce800f01fbe43cbb0ae1601df9
Lait <mr.lait3@gmail.com>
2019-10-08 14:57:24 +0400

    Автоматически реализуемые свойства Name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 public sealed class Employee
 {
-    private String name; // Поле стало закрытым
+    // Это свойство является автоматически реализуемым
+    public String Name { get; set; }
     private Int32 age; // Поле стало закрытым
 
-    public String Name
-    {
-        get { return name; }
-        set { name = value; } // Ключевое слово value
-    }                         // идентифицирует новое значение
+    // идентифицирует новое значение
     public Int32 Age
     {
         get { return age; }
         set
         {
-            if (value < 0)
+            if (value < 0) // Ключевое слово value всегда
+                           // идентифицирует новое значение
             {
                 throw new ArgumentOutOfRangeException("value", value.ToString(),
                     "The value must be greater than or equal to 0");
@@ -30,7 +28,7 @@ public sealed class Programm
         Employee employee = new Employee
         {
             Name = "Ame",
-            Age = -1
+            Age = 1
         };
         Console.WriteLine(employee.Name + " " + employee.Age);
     }
