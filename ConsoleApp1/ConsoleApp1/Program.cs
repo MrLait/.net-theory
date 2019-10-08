a3c807c3a6fdba0fc06de399911e675026a26c12
Lait <mr.lait3@gmail.com>
2019-10-08 14:25:18 +0400

    Не инкапсулированный класс с полями

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,32 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
+public sealed class Employee
+{
+    public String Name;
+    public Int32 Age;
+}
 public sealed class Programm
 {
     public static void Main()
     {
+        Employee employee = new Employee()
+        {
+            Name = "Ame",
+            Age = 21
+        };
+        Console.WriteLine(employee.Name + " " + employee.Age);
     }
-    // Рекомендуется в этом методе использовать параметр слабого типа
-    public void ManipulateItems<T>(IEnumerable<T> collection) { }
-    // Не рекомендуется в этом методе использовать параметр сильного типа
-    public void ManipulateItems<T>(List<T> collection) { }
-    // Рекомендуется в этом методе использовать параметр мягкого типа
-    public void ProcessBytes(Stream someStream){}
-    // Не рекомендуется в этом методе использовать параметр сильного типа
-    public void ProcessBytes(FileStream someStream) {}
-
-    // Рекомендуется в этом методе использовать
-    // сильный тип возвращаемого объекта
-    public FileStream openFile() { return new FileStream("path", FileMode.Open); }
-    // Не рекомендуется в этом методе использовать
-    // слабый тип возвращаемого объекта
-    public Stream OpenFile() { return Stream.Null; }
-    // Гибкий вариант: в этом методе используется
-    // мягкий тип возвращаемого объекта
-    public IList<Stream> GetStringCollection() { return null; }
-    // Негибкий вариант: в этом методе используется
-    // сильный тип возвращаемого объекта
-    public List<Stream> GetStringCollection() { return null; }
 }
 
