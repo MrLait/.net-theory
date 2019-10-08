c593333b9869bcaf110ed3fadb66b49196710b1e
Lait <mr.lait3@gmail.com>
2019-10-08 14:39:21 +0400

    Инкапсулирование прошлого примера, но не красиво

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 public sealed class Employee
 {
-    public String Name;
-    public Int32 Age;
+    private String name; // Поле стало закрытым
+    private Int32 age; // Поле стало закрытым
+
+    public String GetName() { return name; } 
+    public void SetName(String value) { name = value; }
+    public Int32 GetAge() { return age; }
+    public void SetAge(Int32 value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException("value", value.ToString(), 
+                "The value must be greater than or equal to 0");
+        }
+            age = value;
+    }
 }
 public sealed class Programm
 {
     public static void Main()
     {
-        Employee employee = new Employee()
-        {
-            Name = "Ame",
-            Age = 21
-        };
-        Console.WriteLine(employee.Name + " " + employee.Age);
+        Employee employee = new Employee();
+        employee.SetName("Ame");
+        employee.SetAge(-1);
+       Console.WriteLine(employee.GetName() + " " + employee.GetAge());
     }
 }
 
