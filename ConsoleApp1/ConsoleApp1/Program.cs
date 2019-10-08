7cab7450f863dd755a68d9ee8d072c01958559d3
Lait <mr.lait3@gmail.com>
2019-10-08 14:53:28 +0400

    Элегантное инкапсулирование при помощи свойств

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,27 +4,35 @@ public sealed class Employee
     private String name; // Поле стало закрытым
     private Int32 age; // Поле стало закрытым
 
-    public String GetName() { return name; } 
-    public void SetName(String value) { name = value; }
-    public Int32 GetAge() { return age; }
-    public void SetAge(Int32 value)
+    public String Name
     {
-        if (value < 0)
+        get { return name; }
+        set { name = value; } // Ключевое слово value
+    }                         // идентифицирует новое значение
+    public Int32 Age
+    {
+        get { return age; }
+        set
         {
-            throw new ArgumentOutOfRangeException("value", value.ToString(), 
-                "The value must be greater than or equal to 0");
-        }
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("value", value.ToString(),
+                    "The value must be greater than or equal to 0");
+            }
             age = value;
+        }
     }
 }
 public sealed class Programm
 {
     public static void Main()
     {
-        Employee employee = new Employee();
-        employee.SetName("Ame");
-        employee.SetAge(-1);
-       Console.WriteLine(employee.GetName() + " " + employee.GetAge());
+        Employee employee = new Employee
+        {
+            Name = "Ame",
+            Age = -1
+        };
+        Console.WriteLine(employee.Name + " " + employee.Age);
     }
 }
 
