88e86818a9ddd8bcda8eebdcd570fcd04e86e4b9
Lait <mr.lait3@gmail.com>
2019-10-14 18:17:34 +0400

    Наследование интерфейсов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,50 +1,49 @@
 ﻿using System;
-public interface IComparable<T>
-{
-    Int32 CompareTo(T others);
-}
-// Объект Point является производным от System.Object
-// и реализует IComparable<T> в Point
-public sealed class Point : IComparable<Point>
-{
-    private Int32 m_x, m_y;
-    public Point(Int32 x, Int32 y)
-    {
-        m_x = x;
-        m_y = y;
-    }
-    // Этот метод реализует IComparable<T> в Point
-    public Int32 CompareTo(Point others)
-    {
-        return Math.Sign(Math.Sqrt(m_x * m_x + m_y * m_y)
-            - Math.Sqrt(others.m_x * others.m_x + others.m_y * others.m_y));
-    }
-    public override string ToString()
-    {
-        return String.Format("{0}, {1}", m_x, m_y);
-    }
-}
 public static class Program
 {
     public static void Main()
     {
-        Point[] points = new Point[]
-        {
-            new Point(1,3),
-            new Point(2,5),
-        };
-        // Вызов метода CompareTo интерфейса IComparable<T> объекта Point
-        if (points[0].CompareTo(points[1]) < 0)
-        {
-            Point tempPoint = points[0];
-            points[0] = points[1];
-            points[1] = tempPoint;
-            Console.WriteLine("Swap point");
-        }
-        Console.WriteLine("Points from closest to (0, 0) to farthest: ");
-        foreach (Point item in points)
-        {
-            Console.WriteLine(item);
-        }
+        /************************* Первый пример *************/
+        Base b = new Base();
+        // Console.WriteLine("Вызов реализации Dispose в типе b: Dispose класса Base"); 
+        b.Dispose();
+        // Console.WriteLine("Вызов реализации Dispose в типе объекта b: Dispose класса Base");
+        ((IDisposable)b).Dispose();
+        /************************* Второй пример ************************/
+        Derived d = new Derived();
+        // Console.WriteLine("Вызов реализации Dispose в типе d: Dispose класса Derived"); 
+        d.Dispose();
+        // Console.WriteLine("Вызов реализации Dispose в типе объекта d: Dispose класса Derived"); 
+        ((IDisposable)d).Dispose();
+        /************************* Третий пример *************************/
+        b = new Derived();
+        // Console.WriteLine("Вызов реализации Dispose в типе b: Dispose класса Base");
+        b.Dispose();
+        // Console.WriteLine("Вызов реализации Dispose в типе объекта b: Dispose класса Derived"); 
+        ((IDisposable)b).Dispose();
+    }
+}
+
+// Этот класс является производным от Object и реализует IDisposable
+internal class Base : IDisposable
+{
+    // Этот метод неявно запечатан и его нельзя переопределить
+    public void Dispose()
+    {
+        Console.WriteLine("Base's Dispose");
+    }
+}
+// Этот класс наследует от Base и повторно реализует IDisposable
+internal class Derived : Base, IDisposable
+{
+    // Этот метод не может переопределить Dispose из Base.
+    // Ключевое слово 'new' указывает на то, что этот метод
+    // повторно реализует метод Dispose интерфейса IDisposable
+    new public void Dispose()
+    {
+        Console.WriteLine("Derived's Dispose)");
+        // ПРИМЕЧАНИЕ: следующая строка кода показывает,
+        // как вызвать реализацию базового класса (если нужно)s
+        //base.Dispose(); 
     }
 }
