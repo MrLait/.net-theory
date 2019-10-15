5490ae3efa28885376d81f4a628e8ad75a7fc961
Lait <mr.lait3@gmail.com>
2019-10-15 17:54:31 +0400

    Совершенствование безопасности типов за счет явной реализации интерфейсных методов ч1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,45 +6,27 @@ public static class Program
 {
     public static void Main()
     {
-        MarioPizzeria mP = new MarioPizzeria();
-        // Эта строка вызывает открытый метод GetMenu класса MarioPizzeria
-        mP.GetMenu();
-        IWindow window = mP;
-        IRestaurant restaurant = mP;
-        window.GetMenu();
-        restaurant.GetMenu();
+        SomeValueType v = new SomeValueType();
+        Object o = new Object();
+        Int32 n = v.CompareTo(v);
+        n = v.CompareTo(o);
     }
+    public interface IComparable
+    {
+        Int32 CompareTo(Object other);
+    }
+    internal struct SomeValueType : IComparable
+    {
+        private Int32 m_x;
+        public SomeValueType(Int32 x)
+        {
+            m_x = x;
+        }
 
-    public interface IWindow
-    {
-        Object GetMenu();
-    }
-    public interface IRestaurant
-    {
-        Object GetMenu();
-    }
-    // Этот тип является производным от System.Object
-    // и реализует интерфейсы IWindow и IRestaurant
-    public sealed class MarioPizzeria : IWindow, IRestaurant
-    {
-        // Реализация метода GetMenu интерфейса IWindow
-        Object IWindow.GetMenu()
+        public Int32 CompareTo(Object other)
         {
-            return null;
-        }
-        // Реализация метода GetMenu интерфейса IRestaurant
-        Object IRestaurant.GetMenu()
-        {
-            return null;
-        }
-        // Метод GetMenu (необязательный),
-        // не имеющий отношения к интерфейсу
-        public Object GetMenu() 
-        {
-            return null;
+            return (m_x - ((SomeValueType)other).m_x);
         }
     }
-
-
 }
 
