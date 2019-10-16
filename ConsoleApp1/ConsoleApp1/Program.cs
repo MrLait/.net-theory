d9d4386bdd964c215158373ed47b19f6180aa298
Lait <mr.lait3@gmail.com>
2019-10-16 15:46:11 +0400

    Использование региональных стандартов при сортировке строк

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,19 +5,19 @@ public static class Program
 {
     public static void Main()
     {
+        String s1 = "Strasse";
+        String s2 = "Straße";
+        Boolean eq;
+        // CompareOrdinal возвращает ненулевое значение
+        eq = String.Compare(s1, s2, StringComparison.Ordinal) == 0;
+        Console.WriteLine("Ordinal comparison: '{0}' {2} '{1}'", s1, s2, eq ? "==" : "!=");
 
-        Boolean Equals(String value, StringComparison comparisonType)
-        static Boolean Equals(String a, String b, StringComparison comparisonType)
-        static Int32 Compare(String strA, String strB, StringComparison comparisonType)
-        static Int32 Compare(string strA, string strB, Boolean ignoreCase, CultureInfo culture)
-        static Int32 Compare(String strA, String strB, CultureInfo culture, CompareOptions options)
-        static Int32 Compare(String strA, Int32 indexA, String strB, Int32 indexB, Int32 length, StringComparison comparisonType)
-        static Int32 Compare(String strA, Int32 indexA, String strB, Int32 indexB, Int32 length, CultureInfo culture, CompareOptions options)
-        static Int32 Compare(String strA, Int32 indexA, String strB, Int32 indexB, Int32 length, Boolean ignoreCase, CultureInfo culture)
-        Boolean StartsWith(String value, StringComparison comparisonType)
-        Boolean StartsWith(String value, Boolean ignoreCase, CultureInfo culture)
-        Boolean EndsWith(String value, StringComparison comparisonType)
-        Boolean EndsWith(String value, Boolean ignoreCase, CultureInfo culture)
+        // Сортировка строк для немецкого языка (de) в Германии (DE)
+        CultureInfo ci = new CultureInfo("de-DE");
+        // Compare возвращает нуль
+        eq = String.Compare(s1, s2, true, ci) == 0;
+        Console.WriteLine("Cultural comparison: '{0}' {2} '{1}'", s1, s2, eq ? "==" : "!=");
+
     }
 }
 
