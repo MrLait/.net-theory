f284ae3d58254f6bf09e67f500e0c46f421a826e
Lait <mr.lait3@gmail.com>
2019-10-18 15:38:01 +0400

    Flag

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,108 +1,32 @@
 ﻿using System;
-using System.Security;
-using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
 public static class Program
 {
-    internal enum Color
+    //[Flags] // Компилятор C# допускает значение "Flags" или "FlagsAttribute"
+    internal enum Actions
     {
-        White, // Присваивается значение 0
-        Red,   // Присваивается значение 1
-        Green, // Присваивается значение 2
-        Blue,  // Присваивается значение 3
-        Orange // Присваивается значение 4
-    }
-    //internal struct Color : System.Enum
-    //{
-    //    // Далее перечислены открытые константы,
-    //    // определяющие символьные имена и значения
-    //    public const Color White = (Color)0;
-    //    public const Color Red = (Color)1;
-    //    public const Color Green = (Color)2;
-    //    public const Color Blue = (Color)3;
-    //    public const Color Orange = (Color)4;
-    //    // Далее находится открытое поле экземпляра со значением переменной Color
-    //    // Код с прямой ссылкой на этот экземпляр невозможен
-    //    public Int32 value__;
-    //}
-
-    internal enum Color1 : byte
-    {
-        White,
-        Red,
-        Green,
-        Blue,
-        Orange
+        None = 0
+        Read = 0x0001,
+        Write = 0x0002,
+        ReadWrite = Actions.Read | Actions.Write,
+        Delete = 0x0004,
+        Query = 0x0008,
+        Sync = 0x0010
     }
 
     public static void Main()
     {
-        Console.WriteLine(Enum.GetUnderlyingType(typeof(Color)));
-        Console.WriteLine(Enum.GetUnderlyingType(typeof(Color1)));
-        Color1 color = Color1.Blue;
-        Console.WriteLine(color);            // "Blue" (Общий формат)
-        Console.WriteLine(color.ToString()); // "Blue" (Общий формат)
-        Console.WriteLine(color.ToString("G")); // "Blue" (Общий формат)
-        Console.WriteLine(color.ToString("D")); // "3" (Десятичный формат)
-        Console.WriteLine(color.ToString("X")); // "03" (Шестнадцатеричный формат)
-        Console.WriteLine(Enum.Format(typeof(Color), 3, "G"));
+        String file = Assembly.GetEntryAssembly().Location;
+        FileAttributes attributes = File.GetAttributes(file);
+        Console.WriteLine("Is {0} hidden? {1}", file, (
+         attributes & FileAttributes.Hidden) != 0);
 
-        Color[] colors = (Color[])Enum.GetValues(typeof(Color));
-        Console.WriteLine("Number of symbols defined: " + colors.Length);
-        Console.WriteLine("Value\tSymbol\n-----\t------");
-        foreach (Color c in colors)
-        {
-            Console.WriteLine("{0,5:D}\t{0:G}", c);
-        }
+        //File.SetAttributes(file, FileAttributes.ReadOnly | FileAttributes.Hidden);
 
-        Color[] colors1 = GetEnumValues<Color>();
-        Console.WriteLine("Number of symbols defined: " + colors1.Length);
-        Console.WriteLine("Value\tSymbol\n-----\t------");
-        foreach (Color c in colors1)
-        {
-            Console.WriteLine("{0,5:D}\t{0:G}", c);
-        }
-
-        Color color1 = (Color)Enum.Parse(typeof(Color), "White", false);
-        Console.WriteLine(color1.ToString());
-        // Так как White2 не определен, генерируется исключение ArgumentException
-        // color1 = (Color)Enum.Parse(typeof(Color), "White2", false);
-        Console.WriteLine(color1.ToString());
-
-        // Создается экземпляр перечисления Color со значением 1
-        Enum.TryParse<Color>("1", false, out Color color2);
-        // Создается экземпляр перечисления Color со значение 23
-        Enum.TryParse<Color>("23", false, out Color color3);
-
-        // Выводит "True", так как в перечислении Color
-        // идентификатор Red определен как 1
-        Console.WriteLine(Enum.IsDefined(typeof(Color), 1));
-        // Выводит "True", так как в перечислении Color
-        // идентификатор White определен как 0
-        Console.WriteLine(Enum.IsDefined(typeof(Color), "White"));
-        // Выводит "False", так как выполняется проверка с учетом регистра
-        Console.WriteLine(Enum.IsDefined(typeof(Color), "white"));
-        // Выводит "False", так как в перечислении Color
-        // отсутствует идентификатор со значением 10
-        Console.WriteLine(Enum.IsDefined(typeof(Color), 10));
-
-        SetColor((Color)547);
-
-        void SetColor(Color c)
-        {
-            if (!Enum.IsDefined(typeof(Color), c))
-            {
-
-                throw (new ArgumentOutOfRangeException("c", c, "Invalid Color value."));
-            }
-            // Задать цвет, как White, Red, Green, Blue или Orange
-        }
-
+        Actions actions = Actions.Read | Actions.Delete;
+        Console.WriteLine(actions.ToString()); // "Read, Delete"
+        Console.WriteLine(actions.ToString("F")); // "Read, Delete"
     }
-
-        public static TEnum[] GetEnumValues<TEnum>() where TEnum : struct
-    {
-        return (TEnum[])Enum.GetValues(typeof(TEnum));
-    }
-
 
 }
