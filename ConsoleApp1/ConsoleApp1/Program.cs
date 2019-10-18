cf3cfac231db0abe0c4c859fb0a5f28d28e52995
Lait <mr.lait3@gmail.com>
2019-10-18 17:06:51 +0400

    Одномерные массивы

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,52 +1,22 @@
 ﻿using System;
 using System.IO;
 
-internal static class FileAttributesExtensionMethods
-{
-    public static Boolean IsSet(this FileAttributes flags, FileAttributes flagToTest)
-    {
-        if (flagToTest == 0)
-            throw new ArgumentOutOfRangeException("flagToTest", "Value must not be 0");
-        return (flags & flagToTest) == flagToTest;
-    }
-    public static Boolean IsClear(this FileAttributes flags, FileAttributes flagToTest)
-    {
-        if (flagToTest == 0)
-            throw new ArgumentOutOfRangeException("flagToTest", "Value must not be 0");
-        return !IsSet(flags, flagToTest);
-    }
-    public static Boolean AnyFlagsSet(this FileAttributes flags, FileAttributes testFlags)
-    {
-        return ((flags & testFlags) != 0);
-    }
-    public static FileAttributes Set(this FileAttributes flags, FileAttributes setFlags)
-    {
-        return flags | setFlags;
-    }
-    public static FileAttributes Clear(this FileAttributes flags, FileAttributes clearFlags)
-    {
-        return flags & ~clearFlags;
-    }
-    public static void ForEach(this FileAttributes flags, Action<FileAttributes> processFlag)
-    {
-        if (processFlag == null) 
-            throw new ArgumentNullException("processFlag");
-        for (UInt32 bit = 1; bit != 0; bit <<= 1)
-        {
-            UInt32 temp = ((UInt32)flags) & bit;
-            if (temp != 0) processFlag((FileAttributes)temp);
-        }
-    }
-}
 public static class Program
 {
-    
     public static void Main()
     {
-        FileAttributes fa = FileAttributes.System;
-        fa = fa.Set(FileAttributes.ReadOnly);
-        fa = fa.Clear(FileAttributes.System);
-        fa.ForEach(f => Console.WriteLine(f));
-    }
+        Int32[] myIntegers; // Объявление ссылки на массив
+        myIntegers = new Int32[100]; // Создание массива типа Int32 из 100 элементов
 
-}
+        Control[] myControls; // Объявление ссылки на массив
+        myControls = new Control[50]; // Создание массива из 50 ссылок
+                                      // на переменную Control
+
+        myControls[1] = new Button();
+        myControls[2] = new TextBox();
+        myControls[3] = myControls[2]; // Два элемента ссылаются на один объект
+        myControls[46] = new DataGrid();
+        myControls[48] = new ComboBox();
+        myControls[49] = new Button();
+
+    }
