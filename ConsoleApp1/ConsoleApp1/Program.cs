fd574e53ca25f460dc15acb9fb28731d70a70c63
Lait <mr.lait3@gmail.com>
2019-10-18 17:22:50 +0400

    Нерегулярные (jagged) массивы т.е «массивы массивов»

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Drawing;
 
 public static class Program
 {
     public static void Main()
     {
-        // Создание двухмерного массива типа Doubles
-        Double[,] d = new Double[10, 20];
-        // Создание трехмерного массива ссылок на строки
-        String[,,] s = new String[3, 5, 10];
+        // Создание одномерного массива из массивов типа Point
+        Point[][] myPolygons = new Point[3][];
+        // myPolygons[0] ссылается на массив из 10 экземпляров типа Point
+        myPolygons[0] = new Point[10];
+        // myPolygons[1] ссылается на массив из 10 экземпляров типа Point
+        myPolygons[1] = new Point[20];
+        // myPolygons[3] ссылается на массив из 10 экземпляров типа Point
+        myPolygons[2] = new Point[30];
+        // вывод точек первого многоугольника
+        for (Int32 x = 0; x < myPolygons[0].Length; x++)
+            Console.WriteLine(myPolygons[0][x]);
     }
 }
