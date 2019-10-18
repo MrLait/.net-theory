cb7d0809ab76a1fa8196ea95c0008bff0e4abae1
Lait <mr.lait3@gmail.com>
2019-10-18 17:10:49 +0400

    Многомерные массивы

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.IO;
 
 public static class Program
 {
     public static void Main()
     {
-        Int32[] myIntegers; // Объявление ссылки на массив
-        myIntegers = new Int32[100]; // Создание массива типа Int32 из 100 элементов
-
-        Control[] myControls; // Объявление ссылки на массив
-        myControls = new Control[50]; // Создание массива из 50 ссылок
-                                      // на переменную Control
-
-        myControls[1] = new Button();
-        myControls[2] = new TextBox();
-        myControls[3] = myControls[2]; // Два элемента ссылаются на один объект
-        myControls[46] = new DataGrid();
-        myControls[48] = new ComboBox();
-        myControls[49] = new Button();
-
+        // Создание двухмерного массива типа Doubles
+        Double[,] d = new Double[10, 20];
+        // Создание трехмерного массива ссылок на строки
+        String[,,] s = new String[3, 5, 10];
     }
+}
