df39cc77b4097a3fc964ef595e9c090079dcf36d
Lait <mr.lait3@gmail.com>
2019-10-21 15:10:36 +0400

    Массивы с ненулевой нижней границей

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,20 +7,24 @@ public static class Program
 
     public static void Main()
     {
-        // Пример простого для понимания кода
-        Appointment[] appointments = GetAppointmentsForToday();
-
-        for (Int32 a = 0; a < appointments.Length; a++)
+        // Требуется двухмерный массив [2005..2009][1..4]
+        Int32[] lowerBounds ={ 2005, 1 };
+        Int32[] lengths = { 5, 4 };
+        Decimal[,] quarterlyRevenue = (Decimal[,])Array.CreateInstance(typeof(Decimal), lengths, lowerBounds);
+        Console.WriteLine("{0,4} {1,5} {2,7} {3,7} {4,7}", "Year", "Q1", "Q2", "Q3", "Q4");
+        Int32 firstYear = quarterlyRevenue.GetLowerBound(0);
+        Int32 lastYear = quarterlyRevenue.GetUpperBound(0);
+        Int32 firstQuarter = quarterlyRevenue.GetLowerBound(1);
+        Int32 lastQuarter = quarterlyRevenue.GetUpperBound(1);
+        for (int year = firstYear; year <= lastYear; year++)
         {
-        }
-
-        // Пример более сложного кода
-        Appointment[] appointments = GetAppointmentsForToday();
-        if (appointments != null)
-        {
-            for (Int32 a = 0, a < appointments.Length; a++)
+            Console.Write(year + " ");
+            for (int quarter = firstQuarter; quarter <= lastQuarter; quarter++)
             {
-                // Выполняем действия с элементом appointments[a]
+                Console.Write("{0,7:C} ", quarterlyRevenue[year, quarter]);
             }
+            Console.WriteLine();
         }
+
+    }
 }
