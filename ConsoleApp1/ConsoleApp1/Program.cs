bef5680c4add717dc19f4f60ac66393ee2cf7d0d
Lait <mr.lait3@gmail.com>
2019-10-21 12:26:13 +0400

    Приведение типов в массивах

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,30 +1,64 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 
 public static class Program
 {
+    internal sealed class MyValueType : IComparable
+    {
+        public Int32 CompareTo(Object o)
+        {
+            return -1;
+        }
+    }
+
     public static void Main()
     {
-        String[] names = new String[] {"Aidan", "Grant" };
-        // Использование локальной переменной неявного типа:
-        var names1 = new String[] { "Aidan", "Grant" };
-        var names2 = new[] { "Aidan", "Grant", null };
+        // Создание двухмерного массива FileStream
+        FileStream[,] fs2dim = new FileStream[3,5];
+        // Неявное приведение к массиву типа Object
+        Object[,] o2dim = fs2dim;
+        // Невозможно приведение двухмерного массива к одномерному
+        // Ошибка компиляции CS0030: невозможно преобразовать тип 'object[*,*]'
+        // в 'System.IO.Stream[]'
+        
+        //Stream[] s1dim = (Stream[])o2dim;
 
-        // Ошибочное задание типа массива с помощью локальной
-        // переменной неявного типа
-        //var names3 = new[] { "Aidan", "Grant", 123 };
+        // Явное приведение к двухмерному массиву Stream
+        Stream[,] s2dim = (Stream[,])o2dim;
 
-        String[] names4 = { "Aidan", "Grant" };
-        // Ошибочное использование локальной переменной
-        //var names5 = { "Aidan", "Grant" };
+        // Явное приведение к двухмерному массиву String
+        // Компилируется, но во время выполнения
+        // возникает исключение InvalidCastException
+        
+        //String[,] st2dim = (String[,])o2dim;
 
-        // Применение переменных и массивов неявно заданного типа,
-        // а также анонимного типа:
-        var kids = new[] { new { Name = "Aidan" }, new { Name = "Grant" } };
-        // Пример применения (с другой локальной переменной неявно заданного типа):
-        foreach (var kid in kids)
-        {
-            Console.WriteLine(kid.Name);
-        }
+        // Создание одномерного массива Int32 (значимый тип)
+        Int32[] i1dim = new Int32[5];
+        // Невозможно приведение массива значимого типа
+        // Ошибка компиляции CS0030: невозможно преобразовать
+        // тип 'int[]' в 'object[]'
+        //Object[] o1dim = (Object[])i1dim;
+
+        // Создание нового массива и приведение элементов к нужному типу
+        // при помощи метода Array.Copy
+        // Создаем массив ссылок на упакованные элементы типа Int32
+        Object[] ob1dim = new Object[i1dim.Length];
+        Array.Copy(i1dim, ob1dim, i1dim.Length);
+
+        // Создание массива из 100 элементов значимого типа
+        MyValueType[] src = new MyValueType[100];
+        // Создание массива ссылок IComparable
+        IComparable[] dest = new IComparable[src.Length];
+        // Присваивание элементам массива IComparable ссылок на упакованные
+        // версии элементов исходного массива
+        Array.Copy(src,dest,src.Length);
+
+        String[] sa = new String[100];
+        Object[] oa = sa;
+        oa[5] = "Jeff"; // CLR проверяет принадлежность oa к типу String;
+                        // Проверка проходит успешно
+        oa[3] = 5; // CLR проверяет принадлежность oa к типу Int32;
+                   // Генерируется исключение ArrayTypeMismatchException
     }
 }
