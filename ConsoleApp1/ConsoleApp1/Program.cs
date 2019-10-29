fb344de1207c93336a898392b772319d8ff65003
Lait <mr.lait3@gmail.com>
2019-10-29 15:12:05 +0300

    Дополнительные средства управления цепочками делегатов

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,99 +1,71 @@
 ﻿using System;
-using System.Windows.Forms;
-using System.IO;
-// Объявление делегата; экземпляр ссылается на метод
-internal delegate void Feedback(Int32 value);
-
+using System.Text;
+internal sealed class Light
+{
+    // Метод возвращает состояние объекта Light
+    public String SwitchPosition()
+    {
+        return "The light is off";
+    }
+}
+internal sealed class Fan
+{
+    public String Speed()
+    {
+        throw new InvalidOperationException("The fan broke due to overheating");
+    }
+}
+internal sealed class Speaker
+{
+    public String Volume()
+    {
+        return "The volume is loud";
+    }
+}
 public sealed class Program
 {
+    // Определение делегатов, позволяющих запрашивать состояние компонентов
+    private delegate String GetStatus();
     public static void Main()
     {
-        StaticDelegateDemo();
-        InstanceDelegateDemo();
-        ChainDelegateDemo1(new Program());
-        ChainDelegateDemo2(new Program());
-
+        // Объявление пустой цепочки делегатов
+        GetStatus getStatus = null;
+        // Создание трех компонентов и добавление в цепочку
+        // методов проверки их состояния
+        getStatus += new GetStatus(new Light().SwitchPosition);
+        getStatus += new GetStatus(new Fan().Speed);
+        getStatus += new GetStatus(new Speaker().Volume);
+        Console.WriteLine(GetComponentStatusReport(getStatus));
     }
-
-    private static void StaticDelegateDemo()
+    private static String GetComponentStatusReport(GetStatus status)
     {
-        Console.WriteLine("--StaticDelegateDemo--");
-        Counter(1, 3, null);
-        Counter(1, 3, new Feedback(Program.FeedbackToConsole));
-        Counter(1, 3, new Feedback(FeedbackToMsgBox));
-    }
-
-
-    private static void InstanceDelegateDemo()
-    {
-        Console.WriteLine("----- Instance Delegate Demo -----");
-        Program p = new Program();
-        Counter(1, 3, new Feedback(p.FeedbackToFile));
-    }
-
-
-
-    private static void ChainDelegateDemo1(Program p)
-    {
-        Console.WriteLine("----- Chain Delegate Demo 1 -----");
-        Feedback fb1 = new Feedback(FeedbackToConsole);
-        Feedback fb2 = new Feedback(FeedbackToMsgBox);
-        Feedback fb3 = new Feedback(p.FeedbackToFile);
-
-        Feedback fbChain = null;
-        fbChain = (Feedback)Delegate.Combine(fbChain,fb1);
-        fbChain = (Feedback)Delegate.Combine(fbChain,fb2);
-        fbChain = (Feedback)Delegate.Combine(fbChain,fb3);
-        Counter(1, 2, fbChain);
-        
-        Console.WriteLine();
-        fbChain = (Feedback)Delegate.Remove(fbChain, new Feedback(FeedbackToMsgBox));
-        Counter(1, 2, fbChain);
-
-        Console.WriteLine();
-        fbChain -= new Feedback(FeedbackToMsgBox);
-        Counter(1, 2, fbChain);
-
-
-    }
-
-    private static void ChainDelegateDemo2(Program p)
-    {
-        Console.WriteLine("----- Chain Delegate Demo 2 -----");
-        Feedback fb1 = new Feedback(FeedbackToConsole);
-        Feedback fb2 = new Feedback(FeedbackToMsgBox);
-        Feedback fb3 = new Feedback(p.FeedbackToFile);
-        Feedback fbChain = null;
-
-        
-        fbChain += fb1;
-        fbChain += fb2;
-        fbChain += fb3;
-        Counter(1, 2, fbChain);
-    }
-    private static void Counter(Int32 from, Int32 to, Feedback fb)
-    {
-        for (int val = from; val <= to; val++)
+        // Если цепочка пуста, ничего делать не нужно
+        if (status == null) return null;
+        // Построение отчета о состоянии
+        StringBuilder report = new StringBuilder();
+        // Создание массива из делегатов цепочки
+        Delegate[] arrayOfDelegates = status.GetInvocationList();
+        // Циклическая обработка делегатов массива
+        foreach (GetStatus getStatus in arrayOfDelegates)
         {
-            // Если указаны методы обратного вызова, вызываем их
-            if (fb != null)
-                fb(val);
+            try
+            {
+                // Получение строки состояния компонента и добавление ее в отчет
+                report.AppendFormat("{0}{1}{1}", getStatus(), Environment.NewLine);
+            }
+            catch (InvalidOperationException e)
+            {
+                // В отчете генерируется запись об ошибке для этого компонента
+                Object component = getStatus.Target;
+                report.AppendFormat(
+                "Failed to get status from {1}{2}{0} Error: {3}{0}{0}",
+                Environment.NewLine,
+                ((component == null) ? "" : component.GetType() + "."),
+                getStatus.Method.Name,
+                e.Message);
+            }
         }
-    }
-    private static void FeedbackToConsole(Int32 value)
-    {
-        MessageBox.Show("Item" + value);
-    }
-
-    private static void FeedbackToMsgBox(Int32 value)
-    {
-        MessageBox.Show("Item" + value);
-    }
-    private void FeedbackToFile(Int32 value)
-    {
-        using (StreamWriter sw = new StreamWriter("Status", true))
-        {
-            sw.WriteLine("Item=" + value);
-        }
+        // Возвращение сводного отчета вызывающему коду
+        return report.ToString();
     }
 }
