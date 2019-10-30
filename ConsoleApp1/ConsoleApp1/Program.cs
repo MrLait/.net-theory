341aaaaf4dda4909e0ab8c7a2ad742177b8c3e97
Lait <mr.lait3@gmail.com>
2019-10-30 17:13:57 +0300

    Упаковка null-совместимых значимых типов Распаковка null-совместимых значимых типов Вызов интерфейсных методов через null - совместимый значимый тип

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,38 +4,35 @@ public sealed class Program
 {
     public static void Main()
     {
-        NullCoalescingOperator();
-        Func<String> f = () => SomeMethod() ?? "Untitled";
-        //тоже самое но не так куто
-        Func<String> f = () => {
-            var temp = SomeMethod();
-            return temp != null ? temp : "Untitled";
-        };
+        //Упаковка null-совместимых значимых типов
+        Int32? n1 = null;
+        Object o1 = n1;
+        Console.WriteLine(o1==null);
+        n1 = 5;
+        o1 = n1; // o ссылается на упакованный тип Int32
+        Console.WriteLine("o's type={0}", o1.GetType()); // "System.Int32"
 
-        String s = SomeMethod1() ?? SomeMethod2() ?? "Untitled";
-        //тоже самое но не так куто
-        String s;
-        var sm1 = SomeMethod1();
-        if (sm1 != null) s = sm1;
-        else
-        {
-            var sm2 = SomeMethod2();
-            if (sm2 != null) s = sm2;
-            else s = "Untitled";
-        }
+        //Распаковка null-совместимых значимых типов
+        // Создание упакованного типа Int32
+        Object o = 5;
+        // Распаковка этого типа в Nullable<Int32> и в Int32
+        Int32? a = (Int32?)o; // a = 5
+        Int32 b = (Int32)o; // b = 5
+                            // Создание ссылки, инициализированной значением null
+        o = null;
+        // "Распаковка" ее в Nullable<Int32> и в Int32
+        a = (Int32?)o; // a = null
+        //b = (Int32)o; // NullReferenceException
 
-    }
-    private static void NullCoalescingOperator()
-    {
-        Int32? b = 5;
-        // Приведенная далее инструкция эквивалентна следующей:
-        // x = (b.HasValue) ? b.Value : 123
-        Int32 x = b ?? 123;
-        Console.WriteLine(x);
+        //Вызов метода GetType через null - совместимый значимый тип
+        Int32? x = 5;
+        // Эта строка выводит "System.Int32", а не "System.Nullable<Int32>"
+        Console.WriteLine(x.GetType());
 
-        // Приведенная далее в инструкции строка эквивалентна следующему коду:
-        // String temp = GetFilename();
-        // filename = (temp != null) ? temp : "Untitled";
-        //String filename = GetFilename() ?? "Untitled";
+        //Вызов интерфейсных методов через null - совместимый значимый тип
+        Int32? x1 = 5;
+        Int32 result = ((IComparable)x1).CompareTo(5);
+        Console.WriteLine(result);
+        Int32 result2 = ((IComparable)(Int32)x1).CompareTo(5); // Громоздкий код
     }
 }
