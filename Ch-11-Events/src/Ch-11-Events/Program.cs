d4d71e1dd4ca5aaa6feeccd6316519807d64f700
Lait <mr.lait3@gmail.com>
2020-09-15 19:27:34 +0300

    Realized MailManager

---
+++ Ch-11-Events/src/Ch-11-Events/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ch_11_Events
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MailManager mailManager = new MailManager();
+            Fax fax = new Fax(mailManager);
+            Pager pager = new Pager(mailManager);
+
+            mailManager.SimulateNewMail("A", "B", "T");
+        }
+    }
+}
