fb7d0f86aaa0db0f06f39a36a2375d2927a6d15b
Lait <mr.lait3@gmail.com>
2020-09-17 18:02:32 +0300

    Ch-13-Done

---
+++ Ch-13-Interfaces/src/Ch-13-Interfaces/Interface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ch_13_Interfaces
+{
+    public interface Interface
+    {
+        void M();
+        event EventHandler New;
+        int MyProperty { get; set; }
+        object this[int index] { get; set; }
+
+        /*
+        Interface(){}
+        int MyField;
+        const int MyConst = 10;  
+        static void Mstatic();
+        */
+    }
+}
